553d2236cc88e24c6083414f3b9fd594c41aea93
Benjamin Pontiller <benjamin.pontiller@lnu.se>
2019-06-11 10:01:48 +0200

    Begin to my instaled packages

---
+++ explore_packages.R
@@ -0,0 +1,9 @@
+.libPaths()
+
+library(tidyverse)
+library(here)
+
+ipt <- installed.packages() %>%
+  as_tibble() %>%
+  select(Package, LibPath, Version, Priority, Built) %>%
+  write_csv("../packages-report/installed-packages.csv")
